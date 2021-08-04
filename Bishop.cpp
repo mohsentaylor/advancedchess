4271dfd81e32adbbe0f437640de2bfdc4a54a8e9
unknown <mohsentaghavi2648@gmail.com>
2021-08-04 15:17:19 +0430

    bishop added to the project

---
+++ Bishop.cpp
@@ -0,0 +1,25 @@
+#include "Bishop.h"
+Bishop::Bishop(int a,int b,char x):Chessman(a,b,x)
+{   
+}
+bool Bishop::AreSquaresLegal(int iSrcRow,int iSrcCol,int iDestRow,int iDestCol,Chessman * qpaaBoard[8][8])
+{
+    if ((iDestCol - iSrcCol == iDestRow - iSrcRow) || (iDestCol - iSrcCol == iSrcRow - iDestRow)) 
+    {
+        // Make sure that all invervening squares are empty
+        int iRowOffset = (iDestRow - iSrcRow > 0) ? 1 : -1;
+        int iColOffset = (iDestCol - iSrcCol > 0) ? 1 : -1;
+        int iCheckRow;
+        int iCheckCol;
+        for (iCheckRow = iSrcRow + iRowOffset, iCheckCol = iSrcCol + iColOffset;iCheckRow !=  iDestRow;iCheckRow = iCheckRow + iRowOffset, iCheckCol = iCheckCol + iColOffset)
+        {
+            if (qpaaBoard[iCheckRow][iCheckCol] != 0) 
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
---
+++ Bishop.h
@@ -0,0 +1,18 @@
+#ifndef BISHOP_H
+#define BISHOP_H
+#include "chessman.h"
+class Bishop:public Chessman
+{
+    public:
+        Bishop(int,int,char);
+        Bishop() {}
+    protected:
+        char namad='B';
+        int warn=2;
+        int point=8;
+        bool AreSquaresLegal(int,int,int,int,Chessman* x[8][8]);
+};
+
+    
+#endif
+
